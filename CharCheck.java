c654f389bf936ca6899aa156d816cf82f51de639
Student <sowmiyalv@gmail.com>
2016-07-20 14:47:30 +0530

    CharCheck

---
+++ CharCheck.java
@@ -0,0 +1,17 @@
+
+public class CharCheck
+{
+
+	public static void main(String[] args)
+{
+	char c=args[0].charAt(0);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+	{
+		System.out.println("Char is vowel");
+	}
+	else
+	{ System.out.println("Char is consonant");
+	}
+
+}
+	}
