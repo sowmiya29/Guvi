63851b3ede0565a28fb5fb93c643da54b62df7ba
Student <sowmiyalv@gmail.com>
2016-07-20 13:39:01 +0530

    EvenOrOdd

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+
+public class EvenOrOdd 
+{
+	public static void main(String[] args)
+	{
+		int num=Integer.parseInt(args[0]);
+		if(num%2==0)
+		{
+			System.out.println("Number is even");
+		}
+		else
+			System.out.println("Number is odd");
+}
+}
